b930e3b02506dc657dfd0cf99c5f0d7d8cfddb41
allen <allen@Xubuntu.myguest.virtualbox.org>
2023-07-13 17:15:38 +0800

    delete entire script and create new from scratch
    start from creating a function for each of the operands, and a function
    to call corresponding operands

--- script.js
+++ script.js
@@ -1,63 +1,58 @@
-const result = document.querySelector('.result');
-const feedback = document.querySelector('.feedback');
-const keys = document.querySelectorAll('.key');
-let operator = '';
-let displayNum1 = [];
-let displayNum2 = [];
 let num1 = 0;
 let num2 = 0;
-let flag = false;
+let operand = '';
+
+const numberKeys = document.querySelectorAll('.number.key');
+const operatorKeys = document.querySelectorAll('.operator.key');
+const dotKey = document.querySelector('.dot');
+const clearKey = document.querySelector('.clear');
+const deleteKey = document.querySelector('.delete');
+const equalsKey = document.querySelector('.equals');
+const feedbackScreen = document.querySelector('.feedback')
+const resultScreen = document.querySelector('.result');
 
 
-function getNumber1(e) {
-    displayNum1.push(`${e.target.getAttribute('data-key')}`)
-    result.textContent = displayNum1.join('');
-
-    let temp = displayNum1.join('');
-    num1 = parseFloat(temp);
-}
-
-function getNumber2(e) {
-    displayNum2.push(`${e.target.getAttribute('data-key')}`)
-    result.textContent = displayNum2.join('')
-
-    let temp2 = displayNum2.join('');
-    num2 = parseFloat(temp2);
-}
-
-keys.forEach(key => {
-    key.addEventListener('click', function(e){
-        if (e.target.classList.contains('clear'))
-        {
-            console.log('create code for clear')
-        }
-
-        else if (e.target.classList.contains('delete'))
-        {
-            console.log('create code for delete')
-        }
-
-        else if (e.target.classList.contains('number'))
-        {
-            if (flag === false)
-            {
-                flag = true;
-                getNumber1(e);
-            }
-            else 
-                getNumber2(e);
-        }
-            
-        else if (e.target.classList.contains('operator'))
-        {
-            displayNum1 = [];
-            num2 = num1;
-
-            operator = `${e.target.getAttribute('data-operator')}`
-            feedback.textContent = `${num1} ` + `${operator}`
-        }
-
-        else
-            console.log('equals')
-    })
+numberKeys.forEach(key => {
+    key.addEventListener('click', () => getNumber)
 });
+
+
+
+function operate(operator, a, b){
+    switch(operator)
+    {
+        case '+':
+            add(a, b);
+            break;
+
+        case '-':
+            subtract(a, b);
+            break;
+
+        case 'x':
+            multiply(a, b);
+            break; 
+
+        case '+':
+            divide(a, b);
+            break;
+    }
+}
+
+
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
